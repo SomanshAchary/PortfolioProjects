f2b59aa5809f6ebeee5cd8ba6106fbcdd0101889
Somansh Achary <80352439+SomanshAchary@users.noreply.github.com>
2023-09-08 13:46:25 +0530

    Update Covid- 19 Data Exploration in Sql.sql

--- Covid- 19 Data Exploration in Sql.sql
+++ Covid- 19 Data Exploration in Sql.sql
@@ -20,11 +20,11 @@ order by 1,2
 
 
 -- Total Cases vs Total Deaths
--- Shows likelihood of dying if you contract covid in your country
+-- Shows likelihood of dying if you contract covid in India
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
-Where location like '%states%'
+Where location = 'India'
 and continent is not null 
 order by 1,2
 
@@ -34,7 +34,7 @@ order by 1,2
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+Where location = 'India'
 order by 1,2
 
 
@@ -42,7 +42,7 @@ order by 1,2
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+Where location = 'India'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -51,7 +51,6 @@ order by PercentPopulationInfected desc
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
@@ -64,7 +63,6 @@ order by TotalDeathCount desc
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
@@ -75,7 +73,6 @@ order by TotalDeathCount desc
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 where continent is not null 
 --Group By date
 order by 1,2
